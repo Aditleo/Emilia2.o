8f76a2791106f2673755caccc2154bb5440b0b7e
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-05-27 20:12:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,9 +38,9 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, message.from_user.username, temp.U_NAME))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton("⚙ Bᴏᴛ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ⚙", callback_data="help")
+            InlineKeyboardButton("⚙ Bᴏᴛ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ⚙", url='https://t.me/+aOWzY1w5cZ44MzJl')
             ],[
-            InlineKeyboardButton("💢 ᴄʟɪᴄᴋ ᴛᴏ ᴠɪᴇᴡ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴs 💢", callback_data="help")
+            InlineKeyboardButton("🖥 Mᴏᴠɪᴇ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🖥", callback_data="help")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
